632d9867222d82b8a080e2e4dc1e4bd9a1a15e8e
saket-sontakke <sontakkesaket9@gmail.com>
2025-06-23 14:08:40 +0530

    Home Page, Import text and a view-only document browser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Login from './pages/Login'
 import ForgotPassword from './pages/ForgotPassword'
 import ResetPassword from './pages/ResetPassword'
+import PrivateRoute from './pages/PrivateRoute';
 import Home from './pages/Home'
 import CreateProject from './pages/CreateProject';
 import EditProject from './pages/EditProject';
@@ -20,10 +21,13 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/create-project" element={<CreateProject/>} />
-        <Route path="/edit-project/:id" element={<EditProject />} />
-        <Route path="/project/:id" element={<ProjectView />} />
+
+        {/* Protected routes */}
+        <Route path="/home" element={<PrivateRoute> <Home /> </PrivateRoute>} />
+        <Route path="/create-project" element={<PrivateRoute> <CreateProject/> </PrivateRoute>} />
+        <Route path="/edit-project/:id" element={<PrivateRoute> <EditProject /> </PrivateRoute>} />
+        <Route path="/project/:id" element={<PrivateRoute> <ProjectView /> </PrivateRoute> } />
+
       </Routes>
     </BrowserRouter>
   );
